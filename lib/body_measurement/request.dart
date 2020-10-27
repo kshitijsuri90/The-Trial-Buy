2b3e0be326ab81bb72aad87274779590531eac13
Prakash-sa <sainiprakash525@gmail.com>
2020-10-27 16:42:14 +0530

    beautiful dialog

--- lib/body_measurement/request.dart
+++ lib/body_measurement/request.dart
@@ -5,14 +5,47 @@ import 'package:flutter_myntra_clone/body_measurement/body_measurements.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'dart:async';
+import 'package:awesome_dialog/awesome_dialog.dart';
+import 'package:giffy_dialog/giffy_dialog.dart';
 
 
 Future _showcontent(String size,BuildContext context) {
+
   showDialog(
     context: context, barrierDismissible: false, // user must tap button!
 
     builder: (BuildContext context) {
-      return new AlertDialog(
+      return NetworkGiffyDialog(
+          image:Image.network("https://raw.githubusercontent.com/Prakash-sa/data/master/man_wearing_jacket.gif?token=AINA46VMS64HP7RRTKJERHK7UE2WC",
+            fit: BoxFit.cover,
+          ),
+          title: Text('Size Estimator',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  fontSize: 22.0,
+                  fontWeight: FontWeight.w600)),
+          description:Text('Your size is $size',
+          textAlign: TextAlign.center,
+          ),
+          onOkButtonPressed: () {
+            Navigator.of(context).pop();
+            if (Navigator.canPop(context)) {
+              Navigator.pop(context);
+            } else {
+              SystemNavigator.pop();
+            }
+          },
+        onCancelButtonPressed:() {
+          Navigator.of(context).pop();
+          if (Navigator.canPop(context)) {
+            Navigator.pop(context);
+          } else {
+            SystemNavigator.pop();
+          }
+        },
+         );
+        /*
+        new AlertDialog(
         title: new Text('Size'),
         content: new SingleChildScrollView(
           child: new ListBody(
@@ -35,6 +68,7 @@ Future _showcontent(String size,BuildContext context) {
           ),
         ],
       );
+      */
     },
   );
 }
--- lib/screens/body/body.dart
+++ lib/screens/body/body.dart
@@ -81,178 +81,140 @@ class _BodyState extends State<Body> {
     return Scaffold(
       backgroundColor: Theme.of(context).accentColor,
       body: Builder(
-        builder: (context) =>  SingleChildScrollView(
-          padding:const EdgeInsets.fromLTRB(0, 100, 0, 0) ,
-
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children:<Widget>[
+        builder: (context) =>  Container(
+          child: Center(
+            child:Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                SizedBox(
+                  height: 20.0,
+                ),
                 Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Align(
-                        alignment: Alignment.center,
-                        child: Container(
-                          width: 180,
-                          height: 180,
-                          child: new SizedBox(
-                            width: 180.0,
-                            height: 180.0,
-                            child: (_image1!=null)?Image.file(
-                              _image1,
-                              fit: BoxFit.fill,
-                            ):Image.network(
-                              "https://images.unsplash.com/photo-1502164980785-f8aa41d53611?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
-                              fit: BoxFit.fill,
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    Align(
+                      alignment: Alignment.center,
+                      child: Container(
+                        width: 300,
+                        child: Column(
+                          children: <Widget>[
+                            Align(
+                              alignment: Alignment.centerLeft,
+                              child: TextField(
+                                controller: ageController,
+                                keyboardType: TextInputType.number,
+                                inputFormatters: <TextInputFormatter>[
+                                  FilteringTextInputFormatter.digitsOnly
+                                ],
+                                decoration: InputDecoration(
+                                    border: OutlineInputBorder(),
+                                    labelText: 'Age'
+                                ),
+                              ),
                             ),
-                          ),
+                          ],
                         ),
                       ),
-                      Padding(
-                        padding: EdgeInsets.only(top: 5.0),
-                        child: IconButton(
-                          icon: Icon(
-                            Icons.camera,
-                            size: 30.0,
-                          ),
-                          onPressed: () {
-                            getImage1();
-                          },
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 10.0,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    Align(
+                      alignment: Alignment.center,
+                      child: Container(
+                        width: 300,
+                        child: Column(
+                          children: <Widget>[
+                            Align(
+                              alignment: Alignment.centerLeft,
+                              child:
+                              TextField(
+                                controller: heightController,
+                                keyboardType: TextInputType.number,
+                                inputFormatters: <TextInputFormatter>[
+                                  FilteringTextInputFormatter.digitsOnly
+                                ],
+                                decoration: InputDecoration(
+                                    border: OutlineInputBorder(),
+                                    labelText: 'Height'
+                                ),
+                              ),
+                            ),
+                          ],
                         ),
                       ),
-                    ]
-                  ),
-                ]
-              ),
-
-              SizedBox(
-                height: 20.0,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  Align(
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 10.0,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    Align(
+                      alignment: Alignment.center,
+                      child: Container(
+                        width: 300,
+                        child: Column(
+                          children: <Widget>[
+                            Align(
+                              alignment: Alignment.centerLeft,
+                              child:
+                              TextField(
+                                controller: weightController,
+                                keyboardType: TextInputType.number,
+                                inputFormatters: <TextInputFormatter>[
+                                  FilteringTextInputFormatter.digitsOnly
+                                ],
+                                decoration: InputDecoration(
+                                    border: OutlineInputBorder(),
+                                    labelText: 'Weight'
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                Align(
                     alignment: Alignment.center,
                     child: Container(
-                      width: 100,
-                      child: Column(
-                        children: <Widget>[
-                          Align(
-                            alignment: Alignment.centerLeft,
-                            child: TextField(
-                              controller: ageController,
-                              keyboardType: TextInputType.number,
-                              inputFormatters: <TextInputFormatter>[
-                                FilteringTextInputFormatter.digitsOnly
-                              ],
-                              decoration: InputDecoration(
-                                  labelText: 'Age'
+                      color: Colors.white,
+                      padding: EdgeInsets.all(5),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Container(
+                            width: (MediaQuery.of(context).size.width -
+                                MediaQuery.of(context).padding.left -
+                                MediaQuery.of(context).padding.right) *
+                                0.45,
+                            child: ButtonTheme(
+                              child: PrimaryButton(
+                                title: 'SUBMIT',
+                                onPressed: () => {
+                                  createBodyMeasurement(_age, _height, _weight,context)
+                                },
                               ),
                             ),
                           ),
                         ],
                       ),
-                    ),
-                  ),
-                ],
-              ),
-              SizedBox(
-                height: 10.0,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  Align(
-                    alignment: Alignment.center,
-                    child: Container(
-                      width: 100,
-                      child: Column(
-                        children: <Widget>[
-                          Align(
-                            alignment: Alignment.centerLeft,
-                            child:
-                            TextField(
-                              controller: heightController,
-                              keyboardType: TextInputType.number,
-                              inputFormatters: <TextInputFormatter>[
-                                FilteringTextInputFormatter.digitsOnly
-                              ],
-                              decoration: InputDecoration(
-                                  labelText: 'Height'
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-              SizedBox(
-                height: 10.0,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  Align(
-                    alignment: Alignment.center,
-                    child: Container(
-                      width: 100,
-                      child: Column(
-                        children: <Widget>[
-                          Align(
-                            alignment: Alignment.centerLeft,
-                            child:
-                            TextField(
-                              controller: weightController,
-                              keyboardType: TextInputType.number,
-                              inputFormatters: <TextInputFormatter>[
-                                FilteringTextInputFormatter.digitsOnly
-                              ],
-                              decoration: InputDecoration(
-                                  labelText: 'Weight'
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-              SizedBox(
-                height: 20.0,
-              ),
-              Align(
-                alignment: Alignment.center,
-                child: Container(
-                  color: Colors.white,
-                  padding: EdgeInsets.all(5),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Container(
-                        width: (MediaQuery.of(context).size.width -
-                            MediaQuery.of(context).padding.left -
-                            MediaQuery.of(context).padding.right) *
-                            0.45,
-                        child: ButtonTheme(
-                          child: PrimaryButton(
-                            title: 'SUBMIT',
-                            onPressed: () => {
-                                createBodyMeasurement(_age, _height, _weight,context)
-                            },
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                )
-              ),
-            ],
+                    )
+                ),
+              ],
+            ),
           ),
         ),
       ),
